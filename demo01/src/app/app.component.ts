8ef020ffe9ddedd987c61188e184be7e4da810f2
Jean-Philippe <jprivard@mbp-jprivard.local>
2018-03-15 10:07:38 -0400

    21. Wroking with Component Styles

--- demo01/src/app/app.component.ts
+++ demo01/src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-servers></app-servers>`,
-  styles: []
+  template: `<h3>I'm the App Component</h3><app-servers></app-servers>`,
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
   title = 'app';
