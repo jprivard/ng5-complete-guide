0fe8e25eb73679a29fe0955e27a5f5432ba0877e
Jean-Philippe <jprivard@mbp-jprivard.local>
2018-03-15 10:12:34 -0400

    22. Fully Understand the Component Selector

--- demo01/src/app/app.component.ts
+++ demo01/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<h3>I'm the App Component</h3><app-servers></app-servers>`,
+  //template: `<h3>I'm the App Component</h3><div class="app-servers"></div>`,
   //styleUrls: ['./app.component.css']
   styles: [`
   h3 {
